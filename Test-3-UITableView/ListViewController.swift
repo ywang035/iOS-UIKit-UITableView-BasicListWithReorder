d300ec0ebd4b38d7b6de1157f77d4469792af9a6
Y. WANG <49180491+ywang035@users.noreply.github.com>
2021-11-04 15:07:43 +1100

    able to reorder list

---
+++ Test-3-UITableView/ListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ListViewController.swift
+//  Test-3-UITableView
+//
+//  Created by Yang W on 4/11/21.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+    var characters = ["Link", "Zelda", "Ganondorf", "Midna"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characters.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = characters[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let movedObject = self.characters[sourceIndexPath.row]
+        characters.remove(at: sourceIndexPath.row)
+        characters.insert(movedObject, at: destinationIndexPath.row)
+    }
+}
+
--- Test-3-UITableView/ViewController.swift
+++ Test-3-UITableView/ViewController.swift
@@ -7,13 +7,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationBarDelegate {
+    
+    let editButton = UIButton()
+    let tableViewController = ListViewController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+
+        buildEditButton()
+        buildTableView()
+        
     }
-
-
+    
+    func buildEditButton() {
+        view.addSubview(editButton)
+        
+        editButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        editButton.setTitle("Edit", for: .normal)
+        editButton.setTitleColor(.black, for: .normal)
+        editButton.addTarget(self, action: #selector(editTable), for: .touchUpInside)
+        
+        editButton.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
+        editButton.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor).isActive = true
+        editButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+    }
+    
+    
+    
+    
+    func buildTableView() {
+        view.addSubview(tableViewController.view)
+        
+        tableViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        tableViewController.view.topAnchor.constraint(equalTo: editButton.bottomAnchor).isActive = true
+        tableViewController.view.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor).isActive = true
+        tableViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+    
+    
+    @objc func editTable() {
+        if tableViewController.tableView.isEditing {
+            editButton.setTitle("Edit", for: .normal)
+            tableViewController.tableView.isEditing = false
+            
+        } else {
+            editButton.setTitle("Done", for: .normal)
+            tableViewController.tableView.isEditing = true
+        }
+        
+    }
+    
 }
+
 
